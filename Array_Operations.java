376bcfa2c1fc8fa37a7d2f773549dd9b41974eab
shubh-more <moreshubham456@gmail.com>
2025-05-01 19:27:01 -0500

    Adding array operations

---
+++ Array_Operations.java
@@ -0,0 +1,333 @@
+package algs12;
+
+import stdlib.StdOut;
+import java.util.*; // for comparing arrays in main() tests only
+
+class HW1 {
+
+	/*
+	 * Sum the values of the array.  
+	 * Be careful, the array may have length 0.  In this case, sum should return 0
+	 * 
+	 */
+
+//1
+	public static double sum (double[] a) {
+		// TODO
+		
+		double sumTotal =0.0;
+		for (int count=0;count < a.length;count++) {
+			sumTotal = sumTotal + a[count];
+		}
+		return sumTotal;
+	  }
+
+	/*
+	 *  Returns true if all the elements of the array equal x
+	 * Be careful, the array may have length 0.  In this case, In this case, the value returned  should be true
+	 * 
+2	 */
+	public static boolean all (int[] a, int x) {
+		// TODO
+        for (int i =0;i<a.length;i++){
+        if (a[i] != x){
+
+		return false;
+}
+
+	  }
+return true;
+}
+
+	/*
+	 *  Returns if there is any element of the array that equals x
+	 * Be careful, the array may have length 0.  In this case, the value returned  should be false
+	 * 
+3	 */
+	public static boolean  any (int[] a, int x) {
+		// TODO
+            for (int i =0;i<a.length;i++){
+              if (a[i] == x){
+
+		return true;
+               }
+
+	  }
+            return false;
+}
+
+	
+	/*
+	 *  Returns number of elements of the array that equals x
+	 * Be careful, the array may have length 0.  In this case, the value returned  should be 0
+	 * 
+4	 */
+	public static int count (int[] a, int x) {
+		// TODO
+               int  count =0;
+            for (int i =0;i<a.length;i++){
+              if (a[i] == x){
+              count ++;
+              }
+}
+
+		return count;
+	  }
+
+	/*
+	 *  Returns the number of elements of  the array that equal x
+	 * Be careful, the array may have length 0.  In this case, the value returned must be 0
+	 * 
+	 */
+
+	
+
+	/*
+	 * minValue returns the minimum value in an array of doubles. You can assume
+	 * the array is nonempty and has no duplicates. Your solution must go
+	 * through the array exactly once. Your solution must not call any other
+	 * functions. Here are some examples (using "==" informally):
+	 * 
+	 * -7 == minValue(new double[] { -7 }) -7 == minValue(new double[] { 1, -4,
+	 * -7, 7, 8, 11 }) -13 == minValue(new double[] { -13, -4, -7, 7, 8, 11 })
+	 * -9 == minValue(new double[] { 1, -4, -7, 7, 8, 11, -9 })
+5	 */
+	public static double minValue(double[] list) {
+   double minimum =list[0];
+//scan the rest of the array once
+  for (int i =0;i<list.length;i++){
+        if (list[i] < minimum){
+          minimum=list[i];
+          }
+}
+		
+		return minimum;
+	}
+
+	/*
+	 * minPosition returns the position of the minimum value in an array of
+	 * doubles. The first position in an array is 0 and the last is the
+	 * array.length-1. You can assume the array is nonempty and has no
+	 * duplicates. Your solution must go through the array exactly once. Your
+	 * solution must not call any other functions. Here are some examples (using
+	 * "==" informally):
+	 * 
+	 * 0 == minPosition(new double[] { -7 }) 2 == minPosition(new double[] { 1,
+	 * -4, -7, 7, 8, 11 }) 0 == minPosition(new double[] { -13, -4, -7, 7, 8, 11
+	 * }) 6 == minPosition(new double[] { 1, -4, -7, 7, 8, 11, -9 })
+	 */
+	public static int minPosition(double[] list) {
+	double minimum =list[0];
+        int position =0;
+//scan the rest of the array once
+  for (int i =0;i<list.length;i++){
+        if (list[i] < minimum){
+          minimum=list[i];
+          position=i;
+          }
+}
+		
+		return position;
+	}
+
+
+
+	/*
+	 * distanceBetweenMinAndMax returns difference between the minPosition and
+	 * the maxPosition in an array of doubles.
+	 * 
+	 * You can assume the array is nonempty and has no duplicates. Your solution
+	 * must go through the array exactly once. Your solution must not call any
+	 * other functions. Here are some examples (using "==" informally):
+	 * 
+	 * 0 == distanceBetweenMinAndMax(new double[] { -7 }) // -7,-7 are the min
+	 * and max 3 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11
+	 * }), // 7,11 5 == distanceBetweenMinAndMax(new double[] { -13, -4, -7, 7,
+	 * 8, 11 }) // -13,11 1 == distanceBetweenMinAndMax(new double[] { 1, -4,
+	 * -7, 7, 8, 11, -9 }) // -9,11
+	 */
+	public static int distanceBetweenMinAndMax(double[] list) {
+		// TODO
+
+    double minimum = list[0];
+    double maximum = list[0];
+    int minimumPos = 0;
+    int maximumPos = 0;
+    // One‐pass scan to track both min and max (and their positions)
+    for (int i = 1; i < list.length; i++) {
+        if (list[i] < minimum) {
+            minimum = list[i];
+            minimumPos = i;
+        } else if (list[i] > maximum) {
+            maximum = list[i];
+            maximumPos = i;
+        }
+    }
+    // Return the absolute difference in positions
+    return Math.abs(minimumPos - maximumPos);
+}
+
+
+	/*
+	 * numUnique returns the number of unique values in an array of doubles.
+	 * Unlike the previous questions, the array may be empty and it may contain
+	 * duplicate values. Also unlike the previous questions, you can assume the
+	 * array is sorted.
+	 * 
+	 * Your solution must go through the array exactly once. Your solution must
+	 * not call any other functions. Here are some examples (using "=="
+	 * informally):
+	 * 
+	 * 0 == numUnique(new double[] { }) 1 == numUnique(new double[] { 11 }) 1 ==
+	 * numUnique(new double[] { 11, 11, 11, 11 }) 8 == numUnique(new double[] {
+	 * 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 }) 8
+	 * == numUnique(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66,
+	 * 77, 88 })
+	 */
+	public static int numUnique(double[] list) {
+		// TODO
+        if (list.length == 0) {
+        return 0;
+    }
+       int uniqueCount = 1;               // first element is always unique
+    for (int i = 1; i < list.length; i++) {
+        if (list[i] != list[i - 1]) {  // whenever the current differs from previous
+            uniqueCount++;
+        }
+    }
+    return uniqueCount;
+}
+
+	/*
+	 * removeDuplicates returns the number of unique values in an array of
+	 * doubles. You may assume that the list is sorted, as you did for
+	 * numUnique.
+	 * 
+	 * Your solution may call numUnique, but should not call any other
+	 * functions. After the call to numUnique, you must go through the array
+	 * exactly one more time. Here are some examples (using "==" informally):
+	 * 
+	 * new double[] { } == removeDuplicates(new double[] { }) new double[] { 11
+	 * } == removeDuplicates(new double[] { 11 }) == removeDuplicates(new
+	 * double[] { 11, 11, 11, 11 }) new double[] { 11, 22, 33, 44, 55, 66, 77,
+	 * 88 } == removeDuplicates(new double[] { 11, 11, 11, 11, 22, 33, 44, 44,
+	 * 44, 44, 44, 55, 55, 66, 77, 88, 88 }) == removeDuplicates(new double[] {
+	 * 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 })
+	 */
+	public static double[] removeDuplicates(double[] list) {
+     // 1. Figure out how many uniques there are
+        int n = numUnique(list);
+    // 2. Allocate the result
+    double[] result = new double[n];
+    // 3. If empty, just return
+    if (n == 0) {
+        return result;
+    }
+    // 4. Copy the first element, then scan once to pull in each new value
+    result[0] = list[0];
+    int j = 1;  // next write position in result
+    for (int i = 1; i < list.length; i++) {
+        if (list[i] != list[i - 1]) {
+            result[j++] = list[i];
+        }
+    }
+    return result;
+}
+
+
+  public static void main(String[] args) { double[] r = removeDuplicates(new
+  double[] { 11,22,33}); StdOut.println(r);
+  StdOut.println(Arrays.toString(r));
+
+  
+  }
+ 
+	// You can write tests here to make sure your functions work.
+	public static void main1(String[] args) {
+        // WRITE YOUR OWN TESTS HERE FOR SUM, ALL, any, COUNT
+		StdOut.println("Tests for SUM");
+		StdOut.print("array of size 0");
+		StdOut.println(0==sum(new double[0]));
+		StdOut.print("array of size 2 [1,2]");
+		StdOut.println(3==sum(new double[] {1,2}));
+		
+
+		// test minValue( double [] )
+		StdOut.println("minValue:");
+		StdOut.print("-7 == minValue(new double[] { -7 }) ");
+		StdOut.println(-7 == minValue(new double[] { -7 }));
+
+		StdOut.print("-7 == minValue(new double[] { 1, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(-7 == minValue(new double[] { 1, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("-13 == minValue(new double[] { -13, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(-13 == minValue(new double[] { -13, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("-9 == minValue(new double[] { 1, -4, -7, 7, 8, 11, -9 }) ");
+		StdOut.println(-9 == minValue(new double[] { 1, -4, -7, 7, 8, 11, -9 }));
+
+		// test minPosition( double[] )
+		StdOut.println("\nminPosition:");
+		StdOut.print("0 == minPosition(new double[] { -7 }) ");
+		StdOut.println(0 == minPosition(new double[] { -7 }));
+
+		StdOut.print("2 == minPosition(new double[] { 1, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(2 == minPosition(new double[] { 1, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("0 == minPosition(new double[] { -13, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(0 == minPosition(new double[] { -13, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("6 == minPosition(new double[] { 1, -4, -7, 7, 8, 11, -9 }) ");
+		StdOut.println(6 == minPosition(new double[] { 1, -4, -7, 7, 8, 11, -9 }));
+
+		// test distanceBetweenMinAndMax( double[])
+		StdOut.println("\ndistanceBetweenMinAndMax:");
+		StdOut.print("0 == distanceBetweenMinAndMax(new double[] { -7 }) ");
+		StdOut.println(0 == distanceBetweenMinAndMax(new double[] { -7 }));
+
+		StdOut.print("3 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(3 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("5 == distanceBetweenMinAndMax(new double[] { -13, -4, -7, 7, 8, 11 }) ");
+		StdOut.println(5 == distanceBetweenMinAndMax(new double[] { -13, -4, -7, 7, 8, 11 }));
+
+		StdOut.print("1 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11, -9 }) ");
+		StdOut.println(1 == distanceBetweenMinAndMax(new double[] { 1, -4, -7, 7, 8, 11, -9 }));
+	
+		// test numUnique( double[] )
+		StdOut.println("\nnumUnique:");
+		StdOut.print("0 == numUnique(new double[] { }) ");
+		StdOut.println(0 == numUnique(new double[] { }));
+
+		StdOut.print("1 == numUnique(new double[] { 11 }) ");
+		StdOut.println(1 == numUnique(new double[] { 11 }));
+
+		StdOut.print("1 == numUnique(new double[] { 11, 11, 11, 11 }) ");
+		StdOut.println(1 == numUnique(new double[] { 11, 11, 11, 11 }));
+
+		StdOut.print("8 == numUnique(new double[] { 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 }) ");
+		StdOut.println(8 == numUnique(new double[] { 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 }));
+
+		StdOut.print("8 == numUnique(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 }) ");
+		StdOut.println(8 == numUnique(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 }));
+
+		// test removeDuplicates( double[])
+		StdOut.println("\nremoveDuplicates:");
+		StdOut.print("new double[] { } == removeDuplicates(new double[] { }) ");
+		StdOut.println(Arrays.equals(new double[] { }, removeDuplicates(new double[] { })));
+
+		StdOut.print("new double[] { 11 } == removeDuplicates(new double[] { 11 }) ");
+		StdOut.println(Arrays.equals(new double[] { 11 }, removeDuplicates(new double[] { 11 })));
+
+		StdOut.print("new double[] { 11 } == removeDuplicates(new double[] { 11, 11, 11, 11 }) ");
+		StdOut.println(Arrays.equals(new double[] { 11 }, removeDuplicates(new double[] { 11, 11, 11, 11 })));
+
+		StdOut.print("new double[] { 11, 22, 33, 44, 55, 66, 77, 88 } == removeDuplicates(new double[] { 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 }) ");
+		StdOut.println(Arrays.equals(new double[] { 11, 22, 33, 44, 55, 66, 77, 88 }, removeDuplicates(new double[] { 11, 11, 11, 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88, 88 })));
+
+		StdOut.print("new double[] { 11, 22, 33, 44, 55, 66, 77, 88 } == removeDuplicates(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 }) ");
+		StdOut.println(Arrays.equals(new double[] { 11, 22, 33, 44, 55, 66, 77, 88 }, removeDuplicates(new double[] { 11, 22, 33, 44, 44, 44, 44, 44, 55, 55, 66, 77, 88 })));
+
+	}
+}
+
